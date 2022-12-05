e13e930d58b5486587c36b88f3093587318b6194
faruk <farukuzgun@gmail.com>
2022-12-04 23:17:26 -0500

    5Aralık

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import com.sun.xml.internal.fastinfoset.util.CharArray;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Stack;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,11 +18,65 @@ public class Main {
         m6StrArray(str);
         m7ChrArray(str);
         m8bytArray(str);
+        m9arrayList(str);
+        System.out.print("m10recurcion: ");
+        m10recurcion(str);
+        m11stack(str);
+        m12Lambda(str);
+        m13unicode(str);
 
 
 
 
 
+
+    }
+
+    private static void m13unicode(String str) {
+        System.out.println("Unicode ile: "+ "\u202E"+str);
+    }
+
+    private static void m12Lambda(String str) {
+        System.out.println("LAmbda ile: "+Stream.of(str).map(t -> t.split("")).flatMap(Arrays::stream).reduce("", (s, c) -> c + s));
+//        System.out.println("Lambda ile "+str.chars().mapToObj(ch -> Character.toString(ch)).reduce("", (s, c) -> c + s));
+        System.out.println("LAmbda ile2: "+str.chars().mapToObj( ch -> Character.toString ((char) ch)).reduce("", (s, c) -> c + s));
+  //      System.out.println("LAmbda ile2: "+str.chars().map(Character::toString).reduce("", (s, c) -> c + s));
+
+
+    }
+
+    private static void m11stack(String str) {
+        Stack<Character> stack =new Stack<Character>();
+        char [] chr = str.toCharArray();
+        for (int i = 0; i <chr.length ; i++) {
+            stack.push(chr[i]);
+        }
+        int i=0;
+        while (!stack.empty()){
+            chr[i++]= stack.pop();
+        }
+
+        System.out.println("Stack ile: "+String.valueOf(chr));
+        System.out.println("Stack ile: "+new String(chr));
+    }
+
+    private static void m10recurcion(String str) {
+        if (!str.isEmpty()){
+    System.out.print(str.charAt(str.length()-1));
+    m10recurcion(str.substring(0,str.length()-1));
+}else System.out.println();
+
+    }
+
+    private static void m9arrayList(String str) {
+        ArrayList<String> arraySs = new ArrayList<>(Arrays.asList(str.split("")));
+        System.out.println(arraySs);
+        Collections.reverse(arraySs);
+        System.out.println(arraySs);
+        String [] arr = arraySs.toArray(new String[0]);
+        System.out.println(String.join("", arr));
+        System.out.println(String.join("^", arr));
+        System.out.println(String.join("#", arr));
     }
 
     private static void m8bytArray(String str) {
